c5ea245708771cdd31ffa056746d0f57b8a191cb
dszhusb <71653499+dszhusb@users.noreply.github.com>
2024-06-24 01:21:06 -0400

    Visibility Changes

--- src/routes/shells/ParametricMesh.jsx
+++ src/routes/shells/ParametricMesh.jsx
@@ -15,11 +15,12 @@ export default function ParametricMesh() {
 
     const c = useControls("Generating Curve", {
         b: { value: 0.01, min: 0, max: 2, step: 0.01 },
-        d: { value: 1, min: 0, max: 2, step: 0.01 },
+        d: { value: 1, min: 0.01, max: 2, step: 0.01 },
         z: { value: 1, min: 0, max: 20, step: 0.01 }
     })
 
     const e = useControls("Cross Section Ellipse", {
+        cs: { value: 1, min: 1, max: 5, step: 0.01 },
         a: { value: 1, min: 0.5, max: 1.5, step: 0.01 },
         phi: { value: 0, min: 0, max: 2 * Math.PI, step: 0.01 }
     })
@@ -34,7 +35,7 @@ export default function ParametricMesh() {
     const g = useControls("Geometry Settings", {
         detail: { value: 128, min: 2, max: 100, step: 1 },
         sDetail: { value: 20, min: 20, max: 100, step: 1 },
-        s: { value: 11, min: 0, max: 50, step: 1 },
+        s: { value: 12, min: 0, max: 50, step: 1 },
         wireframe: false
     })
 
@@ -46,7 +47,7 @@ export default function ParametricMesh() {
     // })
 
     const geometry = new THREE.CylinderGeometry(1, 1, 1, g.sDetail, g.s * g.detail, true)
-    const curve = getCurvePoints(e.a, c.b, d.c, g.detail, c.z, c.d, g.s, g.sDetail, e.phi, d.n, d.cDepth, d.nDepth)
+    const curve = getCurvePoints(e.a, c.b, d.c, g.detail, c.z, c.d, g.s, g.sDetail, e.phi, d.n, d.cDepth, d.nDepth, e.cs)
     const position = geometry.attributes.position;
 
     for (let i = 0; i < position.count; i += 1) {
@@ -71,13 +72,13 @@ function mapMesh(geometry, wireframe) {
                 color={0xffddd4}
                 iridescence={1}
                 roughness={0}
-                clearcoat={1}
+                clearcoat={0.5}
             />
         </mesh>
     )
 }
 
-function getCurvePoints(a, b, c, detail, z, d, s, sDetail, phi, n, cDepth, nDepth) {
+function getCurvePoints(a, b, c, detail, z, d, s, sDetail, phi, n, cDepth, nDepth, cs) {
     let curve = []
 
     for (let i = 0.1; i < s; i += Math.PI / detail) {
@@ -101,7 +102,7 @@ function getCurvePoints(a, b, c, detail, z, d, s, sDetail, phi, n, cDepth, nDept
             const nCurv = 1 + nDepth * Math.sin(deg * n)
             const x = a * Math.sin(deg) * Math.cos(phi) + Math.cos(deg) * Math.sin(phi)
             const z = a * Math.sin(deg) * Math.sin(phi) - Math.cos(deg) * Math.cos(phi)
-            let point = new THREE.Vector3(x * i * nCurv * ic, 0, z * i * nCurv * ic)
+            let point = new THREE.Vector3(x * i * nCurv * ic * cs, 0, z * i * nCurv * ic * cs)
             point.applyQuaternion(qt)
             point.add(pos)
             curve.push(point.x, point.y, point.z)
